77ef36c62a693a7a071d2c12ace12453352e6698
Emil Manniche <emilmanniche@hotmail.dk>
2020-04-22 11:20:52 +0200

    Initial Commit

---
+++ Emiltest/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Emiltest
+//
+//  Created by Emil Manniche on 22/04/2020.
+//  Copyright © 2020 Emil Manniche. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
